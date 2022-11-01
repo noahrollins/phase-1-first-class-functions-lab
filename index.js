2aa795eed4a42cf378fb43adc3dde8a32405c1a4
Noah Rollins <noahjrollins@gmail.com>
2022-11-01 13:34:04 -0500

    commit tree 3a612af24e94468a063cbbb3b1408b79fd55c932

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function () {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function () {
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    return function (fare){
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers();
+}
